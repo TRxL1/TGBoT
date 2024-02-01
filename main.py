af7e9e4980f51b70b375e9a45ab2156c54b9247c
TRxL1 <Troshkinr1@gmail.com>
2024-02-01 22:17:48 +0400

    init commit Добавил каркас

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-print("Hello,World!")
-read_input = input()
-print(read_input)
 import telebot
 bot = telebot.TeleBot('6373396991:AAGP1IvnXf33liIMm-MJThLh2kk6JR9_pMs')
-@bot.message_handlers(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start(message):
-    bot.send_message(message.chat.id, '<b>Привет<b>', parse_mode='html')
+    bot.send_message(message.chat.id, '<b>Привет</b>', parse_mode='html')
 
 
 
 bot.polling(none_stop=True)
+
+
 
 
 
