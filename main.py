eeec26cc45212ef3cc9aa01a1c4299aa109829e6
TRxL1 <Troshkinr1@gmail.com>
2024-02-01 22:51:41 +0400

    init commit Добавил каркас

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import telebot
 bot = telebot.TeleBot('6373396991:AAGP1IvnXf33liIMm-MJThLh2kk6JR9_pMs')
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
-    mess = f'Привет, <b>{message.from_user.first_name} <u>{message.from_user.last_name} </u></b>'
-    bot.send_message(message.chat.id, '<b>Привет</b>', parse_mode='html')
+    last_name = message.from_user.last_name or ""
+    mess = f'Привет, <b>{message.from_user.first_name} <u>{message.last_name} </u></b>'
+    bot.send_message(message.chat.id, mess, last_name, parse_mode='html')
+
 
 
 
 bot.polling(none_stop=True)
+
+
+
 
 
 
