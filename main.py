188a48992a6efc20de780cd64959f6bff97e5139
TRxL1 <Troshkinr1@gmail.com>
2024-02-01 23:22:08 +0400

    init commit Добавил каркас

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ bot = telebot.TeleBot('6373396991:AAGP1IvnXf33liIMm-MJThLh2kk6JR9_pMs')
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
     last_name = message.from_user.last_name or ""
-    mess = f'Привет, <b>{message.from_user.first_name} <u>{message.last_name} </u></b>'
-    bot.send_message(message.chat.id, mess, last_name, parse_mode='html')
+    mess = f'Привет, <b>{message.from_user.first_name} <u>{last_name} </u></b>'
+    bot.send_message(message.chat.id, mess,  parse_mode='html')
 
 
 
